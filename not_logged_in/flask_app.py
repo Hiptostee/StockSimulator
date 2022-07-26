a683224d7538a01366dd3c47ab1493aab06d2606
Hiptostee <joey.marra2007@gmail.com>
2022-07-26 11:26:53 -0400

    got delete account working

--- not_logged_in/flask_app.py
+++ not_logged_in/flask_app.py
@@ -23,7 +23,7 @@ def home():
 def signup():
     username = request.form.get("name")          #gets username from form
     session["password"] = request.form.get("pass")     #gets password from form and stores to a session variable
-    with open("C:\\Users\\joey_\\OneDrive\\Desktop\\StockSim\\user_pass.json", "r") as file:        #Gets data from the user_pass json file and stores to data
+    with open("C:\\Users\\joey_\\StockSimulator\\user_pass.json", "r") as file:        #Gets data from the user_pass json file and stores to data
         data = json.load(file)
     if session.get("password") in data.keys():          #tries to validate signup information
         if session.get("password") == None:                #if there is no data in the user signup orm, set the output to nothing becuase if there is nothing it defaults to something unwanted
@@ -37,7 +37,7 @@ def signup():
             output = ""
             return render_template("sign_up.html", output = output)
         else:
-            with open("C:\\Users\\joey_\\OneDrive\\Desktop\\StockSim\\user_pass.json", "w") as file:         #if the password is available, save data into user_pass.json as follows: {password:{username:10000}}
+            with open("C:\\Users\\joey_\\StockSimulator\\user_pass.json", "w") as file:         #if the password is available, save data into user_pass.json as follows: {password:{username:10000}}
                 data[session.get("password")] = ({username:10000})
                 json.dump(data, file)
             return render_template("homepage.html")   #redirect to logged in pages after signup is validated
@@ -50,7 +50,7 @@ def about():
 def login():
     username = request.form.get("user_input")                     #gets username from login form
     session["password"] = request.form.get("pass_input")                      #gets password from login form and saves it to a session variable
-    with open("C:\\Users\\joey_\\OneDrive\\Desktop\\StockSim\\user_pass.json", "r") as file:
+    with open("C:\\Users\\joey_\\StockSimulator\\user_pass.json", "r") as file:
         data = json.load(file)                                                    #loads user-pass.json and gathers data
     if session.get("password") in data.keys():                             #if password in the data, then log the user in and redirect to homepage
         return render_template("homepage.html")
@@ -83,7 +83,7 @@ def buy():
     stock_name = request.form.get('user_input_buy')
     amount_of_stock = request.form.get("pass_input_buy")
 
-    with open("C:\\Users\\joey_\\OneDrive\\Desktop\\StockSim\\user_pass.json", "r") as file:
+    with open("C:\\Users\\joey_\\StockSimulator\\user_pass.json", "r") as file:
         data = json.load(file)
 
     if stock_name == None or amount_of_stock == None:
@@ -99,7 +99,7 @@ def buy():
             html = html.strip("\n")
             html = float(html)
             total = round(html * float(amount_of_stock), 2)
-            with open("C:\\Users\\joey_\\OneDrive\\Desktop\\StockSim\\user_pass.json","r") as file:
+            with open("C:\\Users\\joey_\\StockSimulator\\user_pass.json","r") as file:
                 data = json.load(file)
                 for i in data.keys():
                     if i == session.get("password"):
@@ -115,7 +115,7 @@ def buy():
             return render_template("buy.html", error = "Stock Not Found, or Number of Shares Invalid")
 
 
-        with open("C:\\Users\\joey_\\OneDrive\\Desktop\\StockSim\\user_pass.json","r") as file:
+        with open("C:\\Users\\joey_\\StockSimulator\\user_pass.json","r") as file:
             r_dis = requests.get("https://www.marketwatch.com/investing/stock/" + stock_name)    #gathers stock data again and calculates if the user has enough money to perform this transaction, if no then return the suer does not hve enough funds
             html_dis = BeautifulSoup(r_dis.text, 'lxml')
             html = html_dis.find(class_ = "intraday__price")
@@ -131,7 +131,7 @@ def buy():
                         total2 = (j - total)
                         if total2 < 0:
                             return render_template("buy.html", error = "You do not have enough funds")
-        with open("C:\\Users\\joey_\\OneDrive\\Desktop\\StockSim\\user_pass.json","w") as file:         #update user account value after valid transaction
+        with open("C:\\Users\\joey_\\StockSimulator\\user_pass.json","w") as file:         #update user account value after valid transaction
             for i in data.keys():
                 if i == session.get("password"):
                     for k,v in data[i].items():
@@ -139,15 +139,44 @@ def buy():
                         json.dump(data, file)
 
         rando = random.randint(100000,999999)
-        with open("C:\\Users\\joey_\\OneDrive\\Desktop\\StockSim\\data.json", "r") as file:
+        with open("C:\\Users\\joey_\\StockSimulator\\data.json", "r") as file:
             data = json.load(file)
 
-        with open("C:\\Users\\joey_\\OneDrive\\Desktop\\StockSim\\data.json", "w") as file:
+        with open("C:\\Users\\joey_\\StockSimulator\\data.json", "w") as file:
             data[rando] = ({session.get('password'):{str(date.today()):{stock_name:{amount_of_stock:html}}}})         #add user transaction details to data.json
             json.dump(data, file, indent = 2)
 
 
         return render_template("buy.html", total = total_final, stock_symbol = stock_name, price = price, amount = amount_of_stock)      #render buy.html after all values have been satisfied
+
+@app.route('/delete_account.html', methods = ["POST", "GET"])
+def delete():
+    password = request.form.get("user_input")
+    pop_list = []
+    with open("C:\\Users\\joey_\\StockSimulator\\user_pass.json", "r") as file:
+        data = json.load(file)
+    with open("C:\\Users\\joey_\\StockSimulator\\data.json", "r") as file2:
+        data2 = json.load(file2)
+        if password not in data:
+            return render_template("delete_account.html", output = "Password Not Found")
+        else:
+            with open("C:\\Users\\joey_\\StockSimulator\\user_pass.json", "w") as file:
+                data.pop(password)
+                json.dump(data, file)
+            with open("C:\\Users\\joey_\\StockSimulator\\data.json", "w") as file2:
+                for i in data2.keys():
+                    for j in data2[i]:
+                        print(j)
+                        if j == password:
+                            pop_list.append(i)
+                for x in pop_list:
+                    data2.pop(x)
+                json.dump(data2, file2, indent = 2)
+                return render_template("sign_up.html")
+
+                            
+            
+            
 
 
 if __name__ == '__main__':
